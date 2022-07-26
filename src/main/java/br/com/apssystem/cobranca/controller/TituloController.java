09420e2834fc3b38edef5e548b8b9514d7ae8b2b
adsadilson <adilson.curso@yahoo.com.br>
2022-07-26 20:21:28 -0300

    refatoração do layout

--- src/main/java/br/com/apssystem/cobranca/controller/TituloController.java
+++ src/main/java/br/com/apssystem/cobranca/controller/TituloController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,21 +25,23 @@ public class TituloController {
 
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
-		ModelAndView mv = new ModelAndView("titulo/Cadastro");
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		return mv;
 	}
 
-	@PostMapping("/salvar")
-	public String salvar(Titulo titulo) {
+	@PostMapping()
+	public ModelAndView salvar(Titulo titulo) {
 		service.salvar(titulo);
-		return "redirect:/titulos";
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("mensagem", "Título salvo com sucesso!");
+		return mv;
 	}
 
-	@RequestMapping
+	@GetMapping
 	public String pesquisar(Model model) {
 		List<Titulo> list = service.listarTodosTitulos();
 		model.addAttribute("titulos", list);
-		return "Home";
+		return "PesquisaTitulos";
 	}
 
 	@ModelAttribute("todosStatusTitulo")
